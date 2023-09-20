6a575d1c44ceef6a5b45be6824746e1c602202cf
yogadinugroho <yogaadi.work@gmail.com>
2023-09-20 22:40:19 +0700

    menambahkan if kondisional

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 function CounterDisplay({ count }){
-    return (
-        <p>{count}</p>
-    )
+    if (count === 0){
+        return <p>{count}</p>
+    }
+
+    if (count % 5 === 0 && count % 7 === 0){
+        return <p>FizzBuzz</p>
+    }
+
+    if (count % 5 === 0){
+        return <p>Fizz</p>
+    }
+
+    if (count % 7 === 0){
+        return <p>Buzz</p>
+    }
+
+    return <p>{count}</p>
 }
 
 function IncreaseButton({ increase }){
