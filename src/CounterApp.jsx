b5140b6691dd390aa84e27270aec5a817645f7ae
yogadinugroho <yogaadi.work@gmail.com>
2023-09-20 22:36:37 +0700

    buat komponen sampai dengan aplikasi bisa dijalankan

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+function CounterDisplay({ count }){
+    return (
+        <p>{count}</p>
+    )
+}
+
+function IncreaseButton({ increase }){
+    return (
+        <div>
+            <button onClick={increase}>increase</button>
+        </div>
+    )
+}
+
+function ResetButton({ reset }){
+    return (
+        <div>
+            <button onClick={reset}>reset</button>
+        </div>
+    )
+}
+// menggunakan functional componen karena tidak memanfaatkan state.
+
 class CounterApp extends React.Component{
     constructor(props){
         super(props);
@@ -8,12 +31,16 @@ class CounterApp extends React.Component{
         // inisialisasi state
         this.state = {
             count: 0
-        }
+        };
+
+        // binding event handler
+        this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this)
+        this.onResetEventHandler = this.onResetEventHandler.bind(this)
     }
 
     // event handler
     onIncreaseEventHandler(){
-        this.setState(() => {
+        this.setState((previousState) => {
             return {
                 count: previousState.count + 1
             }
@@ -32,10 +59,15 @@ class CounterApp extends React.Component{
     render(){
         return (
             <div>
-                <p>TODO: Selesaikan component-nya</p>
+                <IncreaseButton increase={this.onIncreaseEventHandler} />
+                <CounterDisplay count={this.state.count} />
+                <ResetButton reset={this.onResetEventHandler} />
             </div>
         )
     }
 }
 
 export default CounterApp;
+
+const root = createRoot(document.getElementById('root'));
+root.render(<CounterApp />)
--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM, { createRoot } from 'react-dom/client'
-import CounterApp from './CounterApp'
-import './index.css'
-
-const root = createRoot(document.getElementById('root'));
-root.render(<CounterApp />)
-
